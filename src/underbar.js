5641a3cb8695e5afb74488d082df95bb7621d566
Rebecca Vreeland <rebeccavreeland@gmail.com>
2014-12-30 13:19:12 -0800

    solved _.map. working on _.reduce. Something happened and the SpecRunner isn't loading any of the functions.

--- src/underbar.js
+++ src/underbar.js
@@ -125,7 +125,6 @@
     });
 
     return rejected;
-
   };
 
   // Produce a duplicate-free version of the array.
@@ -140,7 +139,6 @@
     })
 
     return uniqItems;
-
   };
 
 
@@ -149,6 +147,13 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var results = [];
+
+    _.each(collection, function(item) {
+      results.push(iterator(item));
+    });
+
+    return results; 
   };
 
   /*
@@ -190,6 +195,18 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+
+    var accumulator = accumulator === undefined ? collection.shift() : accumulator;
+
+    var len = collection.length;
+
+    for (var i = 0; i < len; i++) {
+      accumulator = iterator(accumulator, i);
+    }
+
+    return accumulator;
+
+    })
   };
 
   // Determine if the array or object contains a given value (using `===`).
